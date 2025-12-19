5d229d38f7ed84c4602363b13679808c2509495a
Arif-Siregar <150424609+Arif-Siregar@users.noreply.github.com>
2025-12-19 15:59:28 +1100

    Attempt on making pwa, adding pwa packages and icons

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,38 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
+       manifest: {
+        name: 'Running App',
+        short_name: 'Running',
+        description: 'Lululemon BOH runners helper',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            "src": "/icons/pwa-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "any"
+          },
+          {
+            "src": "/icons/pwa-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "any"
+          },
+          {
+            "src": "/icons/pwa-maskable-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "maskable"
+          },
+          {
+            "src": "/icons/pwa-maskable-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "maskable"
+          }
+        ]
+      }
     })
   ],
 })
